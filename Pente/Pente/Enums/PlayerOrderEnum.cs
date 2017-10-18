0857656ec978b46566822c4af446cdeee0bf18a2
Kaden Curtis <kdncrts@users.noreply.github.com>
2017-10-18 09:20:23 -0600

    Create PlayerOrderEnum enum that is used in the Player constructor to determine its stone color or the value of it's piece character

---
+++ Pente/Pente/Enums/PlayerOrderEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Enums
+{
+    public enum PlayerOrderEnum
+    {
+        PLAYER1,
+        PLAYER2
+    }
+}
