07eda81462995801b712cb47120ad8d83ad03f98
Kaden Curtis <kdncrts@users.noreply.github.com>
2017-10-18 09:51:20 -0600

    Fixed IsMoveLegal method to work with different sized game boards : Created a struct to save the GameState

--- Pente/Pente/GameProcesses/GameRules.cs
+++ Pente/Pente/GameProcesses/GameRules.cs
@@ -17,15 +17,17 @@ namespace Pente.GameProcesses
         /// <returns>Whether or not the move is legal. </returns>
         public static bool IsMoveLegal(Board board, int[] move, int turnCount)
         {
-            if(turnCount == 1 && move[0] == 9 && move[1] == 9)
+            int rowMiddle = board.rowCount / 2;
+            int colMiddle = board.colCount / 2;
+            if (turnCount == 1 && move[0] == rowMiddle && move[1] == colMiddle)
             {
                 return true;
             }
-            else if (turnCount == 3 && (move[1] < 7 || move[1] > 11) )
+            else if (turnCount == 3 && (move[1] < colMiddle - 2 || move[1] > colMiddle + 2) )
             {
                 return IsMoveLegal(board, move);
             }
-            else if (turnCount == 3 && (move[0] < 7 || move[0] > 11))
+            else if (turnCount == 3 && (move[0] < rowMiddle || move[0] > rowMiddle + 2))
             {
                 return IsMoveLegal(board, move);
             }
---
+++ Pente/Pente/Models/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Models
+{
+    struct GameState
+    {
+        string player1Name;
+        string player2Name;
+
+        int player1CaptureCount;
+        int player2CaptureCount;
+
+        int turnCount;
+
+        string[][] boardState;
+    }
+}
