969c34de18f62a6d1f8e20eaaaa2abc1cd36175e
Noah Tanner <NTanner@student.neumont.edu>
2017-10-20 08:49:53 -0600

    Complet visual representation of the board : Create Cell class for binding use (Every instance of GameBoard neesd to use .TokenXY to get to the string variable) : Update GameRules to check valid moves with a resizable board

---
+++ Pente/Pente/Models/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Models
+{
+    public class Cell : INotifyPropertyChanged
+    {
+        string tokenXY = " ";
+
+        public string TokenXY
+        {
+            get { return tokenXY; }
+            set
+            {
+                tokenXY = value;
+                OnPropertyChanged("tokenXY");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
